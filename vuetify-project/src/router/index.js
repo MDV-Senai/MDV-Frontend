d5e9b03c8177d6712da8ba08a21cedd92bc7a2f7
Matheus <matheusschmoeller@outlook.com>
2024-03-25 20:38:40 -0300

    att

--- vuetify-project/src/router/index.js
+++ vuetify-project/src/router/index.js
@@ -1,15 +1,20 @@
-
-/**
- * router/index.ts
- *
- * Automatic routes for `./src/pages/*.vue`
- */
-
-// Composables
 import { createRouter, createWebHistory } from 'vue-router/auto'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Login',
+      component: () => import('../pages/Login.vue')
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      
+      component: () => import('../pages/Home.vue')
+    }
+  ]
 })
 
 export default router
