60059d8ce1cf72e1821e7e246733686abfd0e5f6
Maria <mariaaantonia133@gmail.com>
2024-04-24 21:58:40 -0300

    Atualização da sidebar

--- vuetify-project/src/components/Header.vue
+++ vuetify-project/src/components/Header.vue
@@ -1,57 +1,94 @@
 <template>
-    <v-toolbar extended color="#FFFFFF">
-      <v-img src="../assets/img/Logo.svg"></v-img>
+  <v-toolbar extended color="#FFFFFF">
+    <v-img src="../assets/img/Logo.svg"></v-img>
 
-      <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
 
-      <v-toolbar-title>Nome do Sistema</v-toolbar-title>
+    <v-toolbar-title>Nome do Sistema</v-toolbar-title>
 
-      <v-spacer></v-spacer>
-      
-      <v-btn icon>
-        <v-icon>mdi-bell-outline</v-icon>
-      </v-btn>
-      <p>Nome do Usuário</p>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
 
-      <v-navigation-drawer
-        v-model="drawer"
-        temporary
-        color="pink-darken-4"
-        location="right"
-      >
+    <v-btn icon>
+      <v-icon>mdi-bell-outline</v-icon>
+    </v-btn>
+    <p>Nome do Usuário</p>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      temporary
+      color="pink-darken-4"
+      location="right"
+    >
+      <v-list-item
+        prepend-icon="mdi-account-circle-outline"
+        title="Nome do Usuário"
+      ></v-list-item>
+
+      <v-list density="compact" nav>
         <v-list-item
-          prepend-icon="mdi-account-circle-outline"
-          title="Nome do Usuário"
+          prepend-icon="mdi-chart-bar"
+          title="Dashboard"
+          value="dashboard"
         ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-information"
+          title="Informações da Organização"
+          value="informacao"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-information"
+          title="Menu de Vagas"
+          value="vagas"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-account-group"
+          title="Administradores"
+          value="administradores"
+        ></v-list-item>
+        <v-list-group value="Actions">
+          <template v-slot:activator="{ props }">
+            <v-list-item v-bind="props" title="Actions"></v-list-item>
+          </template>
 
-        <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-chart-bar" title="Dashboard" value="dashboard"></v-list-item>
-          <v-list-item prepend-icon="mdi-information" title="Informações da Organização" value="informacao"></v-list-item>
-          <v-list-item prepend-icon="mdi-information" title="Menu de Vagas" value="vagas"></v-list-item>
-          <v-list-item prepend-icon="mdi-account-group" title="Administradores" value="administradores"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-    </v-toolbar>
+          <v-list-item
+            v-for="([title, icon], i) in cruds"
+            :key="i"
+            :prepend-icon="icon"
+            :title="title"
+            :value="title"
+          ></v-list-item>
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
+  </v-toolbar>
 </template>
 
 <script>
 export default {
-    data: () => ({
-      drawer: false,
-      group: null,
-    }),
+  data: () => ({
+    drawer: false,
+    group: null,
+     cruds: [
+        ['Cadastrar Instituição', 'mdi-plus-outline'],
+        ['Read', 'mdi-file-outline'],
+        ['Update', 'mdi-update'],
+        ['Delete', 'mdi-delete'],
+      ],
+  }),
 
-    watch: {
-      group () {
-        this.drawer = false
-      },
+  watch: {
+    group() {
+      this.drawer = false;
     },
-  }
+  },
+  open: ["Users"],
+};
 </script>
 
 <style scoped>
-  p {
-    font-size: 15px;
-  }
+p {
+  font-size: 15px;
+}
 </style>
+
