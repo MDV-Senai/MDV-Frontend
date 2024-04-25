312aa7c611c96d983a14248d4f32ce72c671fae2
Matheus <matheusschmoeller@outlook.com>
2024-04-24 21:59:18 -0300

    att

--- vuetify-project/src/pages/Home.vue
+++ vuetify-project/src/pages/Home.vue
@@ -3,15 +3,38 @@
     <v-img src="../assets/img/fundoDesenhos.svg">
       <Header />
 
-      <v-container class="d-flex justify-center align-center"> 
-        <v-card width="2300">
-          <v-card-title>teste</v-card-title>
-        </v-card>
-      </v-container>
-    <Footer/>
+      <div id="fundoCards">
+          <v-row>
+            <v-col>
+              <v-card color="pink-darken-4">
+                  <v-card-title>Teste</v-card-title>
+              </v-card>
+            </v-col>
+
+            <v-col>
+              <v-card color="pink-darken-4">
+                  
+              </v-card>
+            </v-col>
+          </v-row>
+      </div>
+      
+    <Footer />
     </v-img>
   </v-main>
 </template>
 
 <script>
 </script>
+
+<style scoped>
+  #fundoCards {
+    background-color: #FFF;
+    width: 80%;
+    height: 70%;
+    margin: 2% auto;
+    display: flex;
+    /* justify-content: center;
+    align-items: center; */
+  }
+</style>
