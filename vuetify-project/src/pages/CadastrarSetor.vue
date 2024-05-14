4b4cc408f6a46cab6b37d1b7ae2454b7202a3c07
Matheus <matheusschmoeller@outlook.com>
2024-05-14 20:54:01 -0300

    botão

--- vuetify-project/src/pages/CadastrarSetor.vue
+++ vuetify-project/src/pages/CadastrarSetor.vue
@@ -62,51 +62,45 @@
           </v-col>
         </v-row>
 
-        <v-row id="inputResponsivo" class="d-flex justify-center">
-          <v-col cols="12" md="12">
-            <v-text-field
-              label="Atividades"
+        <v-row class="d-flex justify-center">
+          <v-col cols="12" md="3">
+            <div></div>
+          </v-col>
+          <v-col cols="12" md="3">
+            <v-btn
+              @click="reset"
+              class="my-10"
+              color="pink-darken-4"
+              append-icon="mdi-chevron-right"
+              variant="outlined"
+              width="183"
+              height="62"
+            >
+              Limpar
+            </v-btn>
+          </v-col>
+
+          <v-col cols="12" md="3">
+            <v-btn
+              append-icon="mdi-chevron-right"
               variant="outlined"
               color="pink-darken-4"
-            ></v-text-field>
+              class="my-10"
+              width="183"
+              height="62"
+              id="botaoEntrar"
+            >
+              Cadastrar
+
+              <template v-slot:append>
+                <v-icon color="pink-darken-4"></v-icon>
+              </template>
+            </v-btn>
+          </v-col>
+          <v-col cols="12" md="3">
+            <div></div>
           </v-col>
         </v-row>
-
-        <div class="d-flex justify-center">
-          <v-row>
-            <v-col cols="6" md="6">
-              <v-btn
-                @click="reset"
-                class="my-10"
-                color="pink-darken-4"
-                append-icon="mdi-chevron-right"
-                variant="outlined"
-                width="183"
-                height="62"
-              >
-                Limpar
-              </v-btn>
-            </v-col>
-
-            <v-col cols="6" md="6">
-              <v-btn
-                append-icon="mdi-chevron-right"
-                variant="outlined"
-                color="pink-darken-4"
-                class="my-10"
-                 width="183"
-                height="62"
-                id="botaoEntrar"
-              >
-                Cadastrar
-
-                <template v-slot:append>
-                  <v-icon color="pink-darken-4"></v-icon>
-                </template>
-              </v-btn>
-            </v-col>
-          </v-row>
-        </div>
       </v-form>
     </div>
   </v-main>
@@ -115,7 +109,6 @@
 </template>
 
 <style scoped>
-
 #imagem {
   background-image: url("../assets/img/fundoDesenhos.svg");
   background-size: contain;
@@ -143,7 +136,7 @@
 
 #fundoCards {
   background-color: white;
-  border-radius:8px;
+  border-radius: 8px;
   width: 90%;
   margin: 2% auto;
   display: flex;
