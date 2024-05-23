667391918e072931a8dac2695d0e5c158648c0eb
Matheus <matheusschmoeller@outlook.com>
2024-05-22 21:41:41 -0300

    feat: Adicionei db json para puxar ufs.

--- vuetify-project/src/pages/CadastrarInstituicao.vue
+++ vuetify-project/src/pages/CadastrarInstituicao.vue
@@ -13,8 +13,8 @@
             <v-text-field
               label="Instituição"
               clearable
-             class="text-pink-darken-1"
-             color="pink-darken-4"
+              class="text-pink-darken-1"
+              color="pink-darken-4"
             ></v-text-field>
           </v-col>
         </v-row>
@@ -24,8 +24,8 @@
             <v-text-field
               label="Razão Social"
               clearable
-             class="text-pink-darken-1"
-             color="pink-darken-4"
+              class="text-pink-darken-1"
+              color="pink-darken-4"
             ></v-text-field>
           </v-col>
         </v-row>
@@ -34,8 +34,8 @@
             <v-text-field
               label="Diretor"
               clearable
-             class="text-pink-darken-1"
-             color="pink-darken-4"
+              class="text-pink-darken-1"
+              color="pink-darken-4"
             ></v-text-field>
           </v-col>
         </v-row>
@@ -44,8 +44,8 @@
             <v-text-field
               label="Professor Responsável"
               clearable
-             class="text-pink-darken-1"
-             color="pink-darken-4"
+              class="text-pink-darken-1"
+              color="pink-darken-4"
             ></v-text-field>
           </v-col>
         </v-row>
@@ -54,8 +54,8 @@
             <v-text-field
               label="CNPJ"
               clearable
-             class="text-pink-darken-1"
-             color="pink-darken-4"
+              class="text-pink-darken-1"
+              color="pink-darken-4"
             ></v-text-field>
           </v-col>
 
@@ -63,19 +63,19 @@
             <v-text-field
               label="Inscrição Estadual"
               clearable
-             class="text-pink-darken-1"
-             color="pink-darken-4"
+              class="text-pink-darken-1"
+              color="pink-darken-4"
             ></v-text-field>
           </v-col>
         </v-row>
 
         <v-row id="inputResponsivo" class="d-flex justify-center">
           <v-col cols="12" md="4">
-            <v-text-field 
+            <v-text-field
               label="Telefone"
               clearable
-             class="text-pink-darken-1"
-             color="pink-darken-4"
+              class="text-pink-darken-1"
+              color="pink-darken-4"
             ></v-text-field>
           </v-col>
 
@@ -83,16 +83,16 @@
             <v-text-field
               label="Celular"
               clearable
-             class="text-pink-darken-1"
-             color="pink-darken-4"
+              class="text-pink-darken-1"
+              color="pink-darken-4"
             ></v-text-field>
           </v-col>
           <v-col cols="12" md="4">
             <v-text-field
               label="Email"
               clearable
-             class="text-pink-darken-1"
-             color="pink-darken-4"
+              class="text-pink-darken-1"
+              color="pink-darken-4"
             ></v-text-field>
           </v-col>
         </v-row>
@@ -102,25 +102,28 @@
             <v-text-field
               label="CEP"
               clearable
-             class="text-pink-darken-1"
-             color="pink-darken-4"
+              class="text-pink-darken-1"
+              color="pink-darken-4"
             ></v-text-field>
           </v-col>
 
           <v-col cols="12" md="4">
-            <v-select
+            <v-text-field
               label="Cidade"
               clearable
-             class="text-pink-darken-1"
-             color="pink-darken-4"
-            ></v-select>
+              class="text-pink-darken-1"
+              color="pink-darken-4"
+            ></v-text-field>
           </v-col>
           <v-col cols="12" md="2">
             <v-select
+              v-model="selectedUF"
+              :items="ufs"
+              :item-title="'uf'"
+              :item-value="'id'"
               label="UF"
-              clearable
-             class="text-pink-darken-1"
-             color="pink-darken-4"
+              class="text-pink-darken-1"
+              color="pink-darken-4"
             ></v-select>
           </v-col>
 
@@ -128,8 +131,8 @@
             <v-text-field
               label="Nº"
               clearable
-             class="text-pink-darken-1"
-             color="pink-darken-4"
+              class="text-pink-darken-1"
+              color="pink-darken-4"
             ></v-text-field>
           </v-col>
         </v-row>
@@ -139,72 +142,72 @@
             <v-text-field
               label="Rua"
               clearable
-             class="text-pink-darken-1"
-             color="pink-darken-4"
+              class="text-pink-darken-1"
+              color="pink-darken-4"
             ></v-text-field>
           </v-col>
         </v-row>
 
-        <v-row  class="d-flex justify-center">
+        <v-row class="d-flex justify-center">
           <v-col cols="12" md="6">
             <v-text-field
               label="Bairro"
               clearable
-             class="text-pink-darken-1"
-             color="pink-darken-4"
+              class="text-pink-darken-1"
+              color="pink-darken-4"
             ></v-text-field>
           </v-col>
 
           <v-col cols="12" md="6">
             <v-text-field
-             label="Complemento"
-             clearable
-             class="text-pink-darken-1"
-             color="pink-darken-4"
+              label="Complemento"
+              clearable
+              class="text-pink-darken-1"
+              color="pink-darken-4"
             ></v-text-field>
           </v-col>
         </v-row>
 
         <div class="d-flex justify-center">
-           <v-row class="d-flex justify-center">
-          <v-col cols="12" md="3">
-            <div></div>
-          </v-col>
-          <v-col cols="12" md="3">
-            <v-btn
-              @click="reset"
-              class="my-10"
-              color="pink-darken-4"
-              append-icon="mdi-chevron-right"
-              variant="outlined"
-              width="183"
-              height="62"
-            >
-              Limpar
-            </v-btn>
-          </v-col>
+          <v-row class="d-flex justify-center">
+            <v-col cols="12" md="3">
+              <div></div>
+            </v-col>
+            <v-col cols="12" md="3">
+              <v-btn
+                @click="reset"
+                class="my-10"
+                color="pink-darken-4"
+                append-icon="mdi-chevron-right"
+                variant="outlined"
+                width="183"
+                height="62"
+              >
+                Limpar
+              </v-btn>
+            </v-col>
 
-          <v-col cols="12" md="3">
-            <v-btn
-              append-icon="mdi-chevron-right"
-              variant="outlined"
-              color="pink-darken-4"
-              class="my-10"
-              width="183"
-              height="62"
-              id="botaoEntrar"
-            >
-              Cadastrar
+            <v-col cols="12" md="3">
+              <v-btn
+                append-icon="mdi-chevron-right"
+                variant="outlined"
+                color="pink-darken-4"
+                class="my-10"
+                width="183"
+                height="62"
+                id="botaoEntrar"
+              >
+                Cadastrar
 
-              <template v-slot:append>
-                <v-icon color="pink-darken-4"></v-icon>
-              </template>
-            </v-btn>
-          </v-col>
-          <v-col cols="12" md="3">
-            <div></div>
-          </v-col>
-        </v-row>
+                <template v-slot:append>
+                  <v-icon color="pink-darken-4"></v-icon>
+                </template>
+              </v-btn>
+            </v-col>
+            <v-col cols="12" md="3">
+              <div></div>
+            </v-col>
+          </v-row>
         </div>
       </v-form>
     </div>
@@ -214,7 +217,6 @@
 </template>
 
 <style scoped>
-
 #imagem {
   background-image: url("../assets/img/fundoDesenhos.svg");
   background-size: contain;
@@ -237,13 +239,12 @@
 @media (max-width: 768px) {
   #inputResponsivo {
     /* flex-direction: column; */
-   
   }
 }
 
 #fundoCards {
   background-color: white;
-  border-radius:8px;
+  border-radius: 8px;
   width: 90%;
   margin: 2% auto;
   display: flex;
@@ -256,10 +257,30 @@
 
 <script>
 export default {
+  data() {
+    return {
+      ufs: [],
+      selectedUF: null,
+    };
+  },
   methods: {
+    async getUfs() {
+      try {
+        const req = await fetch("http://localhost:3000/items");
+        const data = await req.json();
+        this.ufs = data.ufs;
+        
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    },
     reset() {
       this.$refs.form.reset();
     },
   },
+  mounted() {
+    this.getUfs();
+  },
 };
 </script>
+
