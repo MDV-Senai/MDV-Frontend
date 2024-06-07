6cb94a1d1619aeb725d06a86d8641f9ee0f03afb
Lucas Gabriel Tamanini Demori <lucas-demori2001@hotmail.com>
2024-06-06 21:12:48 -0300

    Feat: adicionando type password no campo senha na tela de login

--- vuetify-project/src/pages/Login.vue
+++ vuetify-project/src/pages/Login.vue
@@ -18,7 +18,7 @@
                       </v-col>
 
                       <v-col cols="12" sm="10">
-                        <v-text-field label="Senha" variant="outlined" color="pink-darken-4"></v-text-field>
+                        <v-text-field label="Senha" type="password" variant="outlined" color="pink-darken-4"></v-text-field>
                       </v-col>
 
                       <div class="d-flex justify-center align-center text-center">
