d97e84ccb9b89f014017a6e59c1a360242d23772
Matheus <matheusschmoeller@outlook.com>
2024-06-26 20:50:56 -0300

    feat: botão target para tela de cadastro de estagiarios + autocomplete para buscas

--- vuetify-project/src/pages/InformacaoVagas.vue
+++ vuetify-project/src/pages/InformacaoVagas.vue
@@ -2,47 +2,65 @@
   <v-main id="imagem">
     <Header />
     <div class="d-flex justify-center align-center">
-      <v-card class="d-flex justify-center align-center" id="card_titulo">Vagas Disponíveis</v-card>
+      <v-card class="d-flex justify-center align-center" id="card_titulo"
+        >Vagas Disponíveis</v-card
+      >
     </div>
     <div id="fundoCards">
-        <div class="d-flex flex-row flex-wrap" id="responsive-cards">
-                <v-col cols="12" md="6" v-for="vaga in vagas" :key="vaga.id">
-                  <v-card>
-                    <v-card-title v-model="titulo">
-                      {{ vaga.titulo }}
-                    </v-card-title>
+      <div class="d-flex flex-row flex-wrap" id="responsive-cards">
+        <v-col cols="12" md="6" v-for="vaga in vagas" :key="vaga.id">
+          <v-card>
+            <v-card-title v-model="titulo">
+              {{ vaga.titulo }}
+            </v-card-title>
 
-                    <v-card-subtitle v-model="quantidade">
-                      {{ vaga.quantidade }}
-                    </v-card-subtitle>
+            <v-card-subtitle v-model="quantidade">
+              {{ vaga.quantidade }}
+            </v-card-subtitle>
 
-                    <v-card-subtitle v-model="turno">
-                      {{ vaga.turno }}
-                    </v-card-subtitle>
+            <v-card-subtitle v-model="turno">
+              {{ vaga.turno }}
+            </v-card-subtitle>
 
-                    <v-card-actions>
-                      <v-btn color="pink-darken-4" text="Descrição"></v-btn>
+            <v-card-actions>
+              <v-btn
+                density="compact"
+                color="pink-darken-4"
+                icon="mdi-plus"
+                :to="routeEstagiario"
+                target="_blank"
+              ></v-btn>
+              <v-btn color="pink-darken-4" text="Descrição"></v-btn>
 
-                      <v-spacer></v-spacer>
+              <v-spacer></v-spacer>
 
-                      <v-btn
-                        :icon="vaga.show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
-                        @click="vaga.show = !vaga.show"
-                      ></v-btn>
-                    </v-card-actions>
+              <v-btn
+                :icon="vaga.show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+                @click="vaga.show = !vaga.show"
+              ></v-btn>
+            </v-card-actions>
 
-                    <v-expand-transition>
-                      <div v-show="vaga.show">
-                        <v-divider></v-divider>
+            <v-expand-transition>
+              <div v-show="vaga.show">
+                <v-divider></v-divider>
 
-                        <v-card-text v-model="descricao">
-                          {{ vaga.descricao }}
-                        </v-card-text>
-                      </div>
-                    </v-expand-transition>
-                  </v-card>
-                </v-col>
+                <v-card-text v-model="descricao">
+                  {{ vaga.descricao }}
+                </v-card-text>
+
+                <v-autocomplete
+                  label="Pesquise"
+                  :items="estagiarios"
+                  :item-title="'titulo'"
+                  :item-value="'id'"
+                  color="pink-darken-4"
+                  class="text-pink-darken-4"
+                ></v-autocomplete>
               </div>
+            </v-expand-transition>
+          </v-card>
+        </v-col>
+      </div>
     </div>
   </v-main>
   <Footer />
@@ -99,6 +117,8 @@ export default {
       descricao: null,
       id: null,
       show: false,
+      routeEstagiario: "/cadastrarEstagiario",
+      estagiarios: null,
     };
   },
 
@@ -115,9 +135,24 @@ export default {
         console.error("Erro :", error);
       }
     },
+
+    async getEstagiarios() {
+      try {
+        const response = await axios.get(
+          "https://run.mocky.io/v3/a6933857-7b64-4915-b159-60d3c1f2fafa"
+        );
+        const info = response.data;
+        const estagiarios = info.items.estagiarios;
+        this.estagiarios = estagiarios;
+        console.log(this.estagiarios);
+      } catch (error) {
+        console.error("Erro :", error);
+      }
+    },
   },
   mounted() {
     this.listarVagas();
+    this.getEstagiarios();
   },
 };
 </script>
