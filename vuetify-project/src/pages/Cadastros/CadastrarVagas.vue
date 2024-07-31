e702642580d12d8f7842c7471dfb2b5e1f772bfb
Matheus <matheusschmoeller@outlook.com>
2024-07-30 21:54:01 -0300

    chore: ajustando o cadastro de vagas

--- vuetify-project/src/pages/Cadastros/CadastrarVagas.vue
+++ vuetify-project/src/pages/Cadastros/CadastrarVagas.vue
@@ -8,15 +8,23 @@
       <v-form ref="form" id="form" class="mx-auto">
         <v-row class="d-flex justify-center mt-8">
           <v-col cols="12" md="12">
-            <v-text-field label="Curso" :rules="[rules.required]" maxlength="255" counter clearable
-              class="text-pink-darken-3" color="pink-darken-4"></v-text-field>
+            <v-select
+              v-model="estagiario"
+              :items="estagiarios"
+              :item-title="'titulo'"
+              :item-value="'id'"
+              label="Curso"
+              class="text-pink-darken-1"
+              color="pink-darken-4"
+              readonly
+            ></v-select>
           </v-col>
         </v-row>
 
         <v-row class="d-flex justify-center">
           <v-col cols="12" md="12">
-            <v-text-field label="Descrição Da Vaga" :rules="[rules.required]" maxlength="255" counter clearable
-              class="text-pink-darken-3" color="pink-darken-4"></v-text-field>
+            <v-textarea label="Descrição Da Vaga" :rules="[rules.required]" maxlength="255" counter clearable
+              class="text-pink-darken-3" color="pink-darken-4"></v-textarea>
           </v-col>
         </v-row>
         <v-row class="d-flex justify-center">
@@ -25,8 +33,8 @@
               class="text-pink-darken-3" color="pink-darken-4"></v-text-field>
           </v-col>
           <v-col cols="12" md="6">
-            <v-text-field label="Turma" :rules="[rules.required]" maxlength="255" counter clearable
-              class="text-pink-darken-3" color="pink-darken-4"></v-text-field>
+            <v-select label="Turno" :rules="[rules.required]" clearable :items="['Matutino', 'Vespertino', 'Noturno', 'Integral']"
+              class="text-pink-darken-3" color="pink-darken-4"></v-select>
           </v-col>
         </v-row>
 
@@ -38,8 +46,8 @@
 
 
           <v-col cols="12" md="6">
-            <v-text-field label="Situação" :rules="[rules.required]" maxlength="255" counter clearable
-              class="text-pink-darken-3" color="pink-darken-4"></v-text-field>
+            <v-select label="Situação" :rules="[rules.required]" clearable :items="['Programada', 'Aberta', 'Encerrada']"
+              class="text-pink-darken-3" color="pink-darken-4"></v-select>
           </v-col>
 
         </v-row>
@@ -118,18 +126,37 @@
 </style>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       rules: {
         required: value => !!value || "Obrigatório.",
       },
+      estagiario: null,
+      estagiarios: [],
     };
   },
   methods: {
     reset() {
       this.$refs.form.reset();
     },
+    async getUfs() {
+      try {
+        const response = await axios.get(
+          "https://run.mocky.io/v3/e1351e5c-3a81-4b3c-8dd3-31f6811e3539"
+        );
+        const data = response.data;
+        const estagiarios = data.items.estagiarios;
+        this.estagiarios = estagiarios;
+        console.log(this.estagiarios);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    },
+  },
+  mounted() {
+    this.getUfs();
   },
 };
 </script>
