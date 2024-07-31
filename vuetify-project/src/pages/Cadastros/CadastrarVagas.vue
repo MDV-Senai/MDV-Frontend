859f2a214f3121f2b4e873aad904bfe5b8abe14b
José Henrique de Oliveira do Patrocinio <henriquepatrocinio98@gmail.com>
2024-07-31 19:44:27 -0300

    style: identação cadastrar vagas

--- vuetify-project/src/pages/Cadastros/CadastrarVagas.vue
+++ vuetify-project/src/pages/Cadastros/CadastrarVagas.vue
@@ -2,46 +2,88 @@
   <v-main id="imagem">
     <Header />
     <div class="d-flex justify-center align-center">
-      <v-card class="d-flex justify-center align-center" id="card_titulo">Cadastro de Vagas</v-card>
+      <v-card class="d-flex justify-center align-center" id="card_titulo"
+        >Cadastro de Vagas</v-card
+      >
     </div>
     <div id="fundoCards">
       <v-form ref="form" id="form" class="mx-auto">
         <v-row class="d-flex justify-center mt-8">
           <v-col cols="12" md="12">
-            <v-text-field label="Curso" :rules="[rules.required]" maxlength="255" counter clearable
-              class="text-pink-darken-3" color="pink-darken-4"></v-text-field>
+            <v-text-field
+              label="Curso"
+              :rules="[rules.required]"
+              maxlength="255"
+              counter
+              clearable
+              class="text-pink-darken-3"
+              color="pink-darken-4"
+            ></v-text-field>
           </v-col>
         </v-row>
 
         <v-row class="d-flex justify-center">
           <v-col cols="12" md="12">
-            <v-text-field label="Descrição Da Vaga" :rules="[rules.required]" maxlength="255" counter clearable
-              class="text-pink-darken-3" color="pink-darken-4"></v-text-field>
+            <v-text-field
+              label="Descrição Da Vaga"
+              :rules="[rules.required]"
+              maxlength="255"
+              counter
+              clearable
+              class="text-pink-darken-3"
+              color="pink-darken-4"
+            ></v-text-field>
           </v-col>
         </v-row>
         <v-row class="d-flex justify-center">
           <v-col cols="12" md="6">
-            <v-text-field label="Setor" :rules="[rules.required]" maxlength="255" counter clearable
-              class="text-pink-darken-3" color="pink-darken-4"></v-text-field>
+            <v-text-field
+              label="Setor"
+              :rules="[rules.required]"
+              maxlength="255"
+              counter
+              clearable
+              class="text-pink-darken-3"
+              color="pink-darken-4"
+            ></v-text-field>
           </v-col>
           <v-col cols="12" md="6">
-            <v-text-field label="Turma" :rules="[rules.required]" maxlength="255" counter clearable
-              class="text-pink-darken-3" color="pink-darken-4"></v-text-field>
+            <v-text-field
+              label="Turma"
+              :rules="[rules.required]"
+              maxlength="255"
+              counter
+              clearable
+              class="text-pink-darken-3"
+              color="pink-darken-4"
+            ></v-text-field>
           </v-col>
         </v-row>
 
         <v-row id="inputResponsivo" class="d-flex justify-center">
           <v-col cols="12" md="6">
-            <v-text-field label="Quantidade de Vagas" :rules="[rules.required]" maxlength="255" counter clearable
-              class="text-pink-darken-3" color="pink-darken-4"></v-text-field>
+            <v-text-field
+              label="Quantidade de Vagas"
+              :rules="[rules.required]"
+              maxlength="255"
+              counter
+              clearable
+              class="text-pink-darken-3"
+              color="pink-darken-4"
+            ></v-text-field>
           </v-col>
-
 
           <v-col cols="12" md="6">
-            <v-text-field label="Situação" :rules="[rules.required]" maxlength="255" counter clearable
-              class="text-pink-darken-3" color="pink-darken-4"></v-text-field>
+            <v-text-field
+              label="Situação"
+              :rules="[rules.required]"
+              maxlength="255"
+              counter
+              clearable
+              class="text-pink-darken-3"
+              color="pink-darken-4"
+            ></v-text-field>
           </v-col>
-
         </v-row>
 
         <div class="d-flex justify-center">
@@ -50,15 +92,29 @@
               <div></div>
             </v-col>
             <v-col cols="12" md="3">
-              <v-btn @click="reset" class="my-10" color="pink-darken-4" append-icon="mdi-chevron-right" variant="outlined"
-                width="183" height="62">
+              <v-btn
+                @click="reset"
+                class="my-10"
+                color="pink-darken-4"
+                append-icon="mdi-chevron-right"
+                variant="outlined"
+                width="183"
+                height="62"
+              >
                 Limpar
               </v-btn>
             </v-col>
 
             <v-col cols="12" md="3">
-              <v-btn append-icon="mdi-chevron-right" variant="outlined" color="pink-darken-4" class="my-10" width="183"
-                height="62" id="botaoEntrar">
+              <v-btn
+                append-icon="mdi-chevron-right"
+                variant="outlined"
+                color="pink-darken-4"
+                class="my-10"
+                width="183"
+                height="62"
+                id="botaoEntrar"
+              >
                 Cadastrar
 
                 <template v-slot:append>
@@ -122,7 +178,7 @@ export default {
   data() {
     return {
       rules: {
-        required: value => !!value || "Obrigatório.",
+        required: (value) => !!value || "Obrigatório.",
       },
     };
   },
