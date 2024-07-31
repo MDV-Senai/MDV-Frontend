6dceb33fb6a721f0545f35832de72a18c0e08a84
José Henrique de Oliveira do Patrocinio <henriquepatrocinio98@gmail.com>
2024-07-31 19:47:21 -0300

    style: identação footer

--- vuetify-project/src/components/Footer.vue
+++ vuetify-project/src/components/Footer.vue
@@ -1,53 +1,53 @@
 <template>
   <v-main class="mt-15">
-
-<v-footer class="d-flex flex-column mt-5 position-absolute bottom-0 w-100" color="pink-accent-1">
-
-
-  <div class="px-4 py-2 d-flex flex-column justify-space-between  text-center  w-100">
-    <strong>Maternidade Darcy Vargas &copy;</strong>
-    <div id="button-team">
-      <v-menu
-        open-on-hover
+    <v-footer
+      class="d-flex flex-column mt-5 position-absolute bottom-0 w-100"
+      color="pink-accent-1"
+    >
+      <div
+        class="px-4 py-2 d-flex flex-column justify-space-between text-center w-100"
       >
-        <template v-slot:activator="{ props }">
-          <v-btn
+        <strong>Maternidade Darcy Vargas &copy;</strong>
+        <div id="button-team">
+          <v-menu open-on-hover>
+            <template v-slot:activator="{ props }">
+              <v-btn color="pink-darken-4" v-bind="props">
+                Equipe de Devs
+              </v-btn>
+            </template>
 
-            color="pink-darken-4"
-            v-bind="props"
-          >
-            Equipe de Devs
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item v-for="(item, index) in items" :key="index">
-            <v-list-item-title>
-              <a class="text-black" target="blank" :href="item.href" v-if="item.href">{{ item.title }}</a>
-            </v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </div>
-  </div>
-</v-footer>
-
+            <v-list>
+              <v-list-item v-for="(item, index) in items" :key="index">
+                <v-list-item-title>
+                  <a
+                    class="text-black"
+                    target="blank"
+                    :href="item.href"
+                    v-if="item.href"
+                    >{{ item.title }}</a
+                  >
+                </v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+      </div>
+    </v-footer>
   </v-main>
 </template>
 
 <script>
-
 </script>
 
 <style scoped>
 a {
-color: white;
-font-weight: bold;
+  color: white;
+  font-weight: bold;
 }
-#button-team{
+#button-team {
   position: absolute;
   bottom: 8px;
-  right:10px;
+  right: 10px;
 }
 </style>
 <script>
@@ -55,11 +55,26 @@ export default {
   data() {
     return {
       items: [
-        { title: 'Maria Antonia', href: 'https://www.linkedin.com/in/maria-antonia-rosa-71502a244?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' },
-        { title: 'Lucas Demori', href: 'https://www.linkedin.com/in/lucas-demori21/' },
-        { title: 'Matheus Trindade', href: 'https://www.linkedin.com/in/matheus-willian-schmoeller-2688b9248/?original_referer=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2F&originalSubdomain=br' },
-        { title: 'Matheus Vicenzi', href: 'https://www.linkedin.com/in/matheus-vicenzi/' },
-        { title: 'José Henrique', href: 'https://www.linkedin.com/in/jose-patrocinio/' },
+        {
+          title: "Maria Antonia",
+          href: "https://www.linkedin.com/in/maria-antonia-rosa-71502a244?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        },
+        {
+          title: "Lucas Demori",
+          href: "https://www.linkedin.com/in/lucas-demori21/",
+        },
+        {
+          title: "Matheus Trindade",
+          href: "https://www.linkedin.com/in/matheus-willian-schmoeller-2688b9248/?original_referer=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2F&originalSubdomain=br",
+        },
+        {
+          title: "Matheus Vicenzi",
+          href: "https://www.linkedin.com/in/matheus-vicenzi/",
+        },
+        {
+          title: "José Henrique",
+          href: "https://www.linkedin.com/in/jose-patrocinio/",
+        },
       ],
     };
   },
