ef21932c11b9aebc773fd4b021eb7a9af204a48a
José Henrique de Oliveira do Patrocinio <henriquepatrocinio98@gmail.com>
2024-08-21 19:31:36 -0300

    feat: padronizando consulta vagas

--- vuetify-project/src/pages/Consultas/ConsultarVagas.vue
+++ vuetify-project/src/pages/Consultas/ConsultarVagas.vue
@@ -1,203 +1,120 @@
 <template>
-  <v-main id="imagem">
+  <div id="imagem" :height="height">
     <Header />
     <div class="d-flex justify-center align-center">
-      <v-card class="d-flex justify-center align-center" id="card_titulo"
-        ><h3>Consultar Vagas</h3></v-card
-      >
+      <v-card class="d-flex justify-center align-center" id="card_titulo">
+        <h3>Consultar Vagas</h3>
+      </v-card>
     </div>
     <div id="fundoCards">
-      <v-container>
-        <v-card flat>
-          <v-spacer></v-spacer>
-
-          <v-text-field
-            v-model="search"
-            label="Pesquise"
-            prepend-inner-icon="mdi-magnify"
-            variant="outlined"
-            color="light-grey-darken-3"
-            hide-details
-            single-line
-          ></v-text-field>
-
-          <v-divider></v-divider>
-          <v-data-table
-            :items="filteredBoats"
-            height="400"
-            item-value="name"
-          >
-            <template v-slot:item="{ item }">
-              <tr>
-                <td>{{ item.name }}</td>
-                <td>{{ item.speed }}</td>
-                <td>
-                  <v-dialog max-width="800">
-                    <template v-slot:activator="{ props: activatorProps }">
-                      <v-btn
-                        v-bind="activatorProps"
-                        density="compact"
-                        icon="mdi-eye-outline"
-                        variant="outlined"
-                        class="light-green-darken-3-var"
-                      ></v-btn>
-                    </template>
-
-                    <template v-slot:default="{ isActive }">
-                      <v-card class="d-flex justify-center text-center">
-                        <div>
-                          <v-row class="mx-5 my-5">
-                            <v-col cols="12" md="12">
-                              <v-text-field
-                                label="Curso"
-                                id="curso"
-                                value="Fármacia"
-                                reandoly
-                                class="text-grey-darken-1"
-                                variant="outlined"
-                                color="light-grey-darken-3"
-                              ></v-text-field>
-                            </v-col>
-                          </v-row>
-                          <v-row class="mx-5 my-5">
-                            <v-col cols="12" md="12">
-                              <v-text-field
-                                label="Descrição da Vaga"
-                                id="descricao"
-                                value=""
-                                reandoly
-                                class="text-grey-darken-1"
-                                variant="outlined"
-                                color="light-grey-darken-3"
-                              ></v-text-field>
-                            </v-col>
-                          </v-row>
-                          <v-row class="mx-5 my-5">
-                            <v-col cols="12" md="6">
-                              <v-text-field
-                                label="Setor"
-                                id="setor"
-                                value="Setor A"
-                                reandoly
-                                class="text-grey-darken-1"
-                                variant="outlined"
-                                color="light-grey-darken-3"
-                              ></v-text-field>
-                            </v-col>
-                            <v-col cols="12" md="6">
-                              <v-text-field
-                                label="Turno"
-                                id="turno"
-                                value="Noturno"
-                                reandoly
-                                class="text-grey-darken-1"
-                                variant="outlined"
-                                color="light-grey-darken-3"
-                              ></v-text-field>
-                            </v-col>
-                          </v-row>
-
-                          <v-row class="mx-5">
-                            <v-col cols="12" md="6">
-                              <v-text-field
-                                label="Quantidade de Vagas"
-                                id="quant_vagas"
-                                value="10"
-                                reandoly
-                                class="text-grey-darken-1"
-                                variant="outlined"
-                                color="light-grey-darken-3"
-                              ></v-text-field>
-                            </v-col>
-                            <v-col cols="12" md="6">
-                              <v-select
-                                label="Situação"
-                                id="situacao"
-                                value=""
-                                reandoly
-                                class="text-grey-darken-1"
-                                variant="outlined"
-                                color="light-grey-darken-3"
-                              ></v-select>
-                            </v-col>
-                          </v-row>
-                        </div>
-                        <v-card-actions>
-                          <v-spacer></v-spacer>
-
-                          <v-btn
-                            text="Fechar"
-                            @click="isActive.value = false"
-                          ></v-btn>
-                        </v-card-actions>
-                      </v-card>
-                    </template>
-                  </v-dialog>
-                  <v-btn
-                    density="compact"
-                    icon="mdi-pencil"
-                    class="mx-5 light-green-darken-3-var"
-                    variant="outlined"
-                  ></v-btn>
-                  <v-btn
-                    density="compact"
-                    icon="mdi-delete"
-                    variant="outlined"
-                    class="light-green-darken-3-var"
-                  ></v-btn>
-                </td>
-              </tr>
-            </template>
-          </v-data-table>
-        </v-card>
-      </v-container>
+      <div class="d-flex">
+        <v-text-field
+          v-model="setor"
+          label="Pesquise"
+          variant="outlined"
+          prepend-inner-icon="mdi-magnify"
+          class="text-grey-darken-4"
+          single-line
+          clearable
+        ></v-text-field>
+      </div>
+      <v-table>
+        <thead>
+          <tr>
+            <th class="text-left">Setor</th>
+            <th class="text-left">Turno</th>
+            <th class="text-left">Situação</th>
+            <th class="text-left">Quantidade</th>
+            <th class="text-center">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in vagaPaginada" :key="item.id">
+            <td class="text-left">{{ item.setor }}</td>
+            <td class="text-left">{{ item.turno }}</td>
+            <td class="text-left">{{ item.situacao }}</td>
+            <td class="text-left">{{ item.quantidade }}</td>
+            <td class="text-center">
+              <VisualizarVaga />
+              <EditarVaga />
+              <DeletarItem />
+            </td>
+          </tr>
+        </tbody>
+      </v-table>
+      <div class="d-flex justify-center mt-4">
+        <v-pagination
+          v-model="pagina"
+          :length="totalPaginas"
+          total-visible="7"
+        ></v-pagination>
+      </div>
     </div>
     <Footer />
-  </v-main>
+  </div>
 </template>
 
 <script>
+import { ref, computed, onMounted, watch } from "vue";
+import { useResponsiveHeight } from "../../composables/useResponsiveHeight.js";
+import { fetchVagas } from "../../services/VagasService.js";
+
 export default {
-  data() {
-    return {
-      search: "",
+  setup() {
+    const { height } = useResponsiveHeight();
+    const vagas = ref([]);
+    const setor = ref("");
+    const pagina = ref(1);
+    const itensPorPagina = ref(10);
+    const vagasFiltradas = ref([]);
 
-      boats: [
-        {
-          name: "Fulano",
-        },
-        {
-          name: "Fulano",
-        },
-        {
-          name: "Fulano",
-        },
-        {
-          name: "Fulano",
-        },
-      ],
+    const loadVagas = async () => {
+      const response = await fetchVagas();
+      vagas.value = response;
+      vagasFiltradas.value = response;
     };
-  },
 
-  computed: {
-    filteredBoats() {
-      if (!this.boats) {
-        return this.virtualBoats;
+    const pesquisarVagas = () => {
+      if (setor.value) {
+        vagasFiltradas.value = vagas.value.filter((vaga) =>
+          vaga.setor.toLowerCase().includes(setor.value.toLowerCase())
+        );
+      } else {
+        vagasFiltradas.value = vagas.value;
       }
-      const searchTerm = this.search.toLowerCase();
-      return this.boats.filter(
-        (boat) =>
-          boat.name.toLowerCase().includes(searchTerm) ||
-          String(boat.speed).includes(searchTerm)
-      );
-    },
-  },
-  methods: {
-    handleButtonClick(item) {},
+      pagina.value = 1;
+    };
+
+    const totalPaginas = computed(() => {
+      return Math.ceil(vagasFiltradas.value.length / itensPorPagina.value);
+    });
+
+    const vagaPaginada = computed(() => {
+      const start = (pagina.value - 1) * itensPorPagina.value;
+      const end = start + itensPorPagina.value;
+      return vagasFiltradas.value.slice(start, end);
+    });
+
+    watch(setor, (newValue) => {
+      pesquisarVagas();
+    });
+
+    onMounted(() => {
+      loadVagas();
+    });
+
+    return {
+      height,
+      setor,
+      pagina,
+      itensPorPagina,
+      totalPaginas,
+      vagaPaginada,
+    };
   },
 };
 </script>
 
 <style lang="scss">
-@import '@/styles/shared';
+@import "@/styles/shared";
 </style>
