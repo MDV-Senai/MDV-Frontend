34dcec43357682c96e605d7b8b4c8de37654af88
José Henrique de Oliveira do Patrocinio <henriquepatrocinio98@gmail.com>
2024-08-22 19:59:25 -0300

    fix: arrumando os mocks

--- vuetify-project/src/services/SetoresService.js
+++ vuetify-project/src/services/SetoresService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export async function fetchSetores() {
   try {
     const response = await axios.get(
-      "https://run.mocky.io/v3/09989b46-41e7-467c-bfa0-4018680e6d44"
+      "https://run.mocky.io/v3/7d49f4ad-732a-422d-b6cf-1e3ad3ea6fac"
     );
     return response.data.setores;
   } catch (error) {
--- vuetify-project/src/services/VagasService.js
+++ vuetify-project/src/services/VagasService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export async function fetchVagas() {
   try {
     const response = await axios.get(
-      "https://run.mocky.io/v3/c082d1c7-e043-48bd-a720-ea9912f097e4"
+      "https://run.mocky.io/v3/abb79b48-d214-4366-83f8-39da34baaad3"
     );
     return response.data.vagas;
   } catch (error) {
