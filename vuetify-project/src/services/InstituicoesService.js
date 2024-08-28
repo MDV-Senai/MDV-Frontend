9cb65250a5596688c94a991b9f0841eab7ba1b41
José Henrique de Oliveira do Patrocinio <henriquepatrocinio98@gmail.com>
2024-08-27 22:02:14 -0300

    feat: adicionando mock instituicao

--- vuetify-project/src/services/InstituicoesService.js
+++ vuetify-project/src/services/InstituicoesService.js
@@ -3,9 +3,9 @@ import axios from "axios";
 export async function fetchInstituicoes() {
   try {
     const response = await axios.get(
-      "https://run.mocky.io/v3/afed03db-4a39-4990-b8cd-1441fe2ff6a2"
+      "http://localhost:3000/Instituicao"
     );
-    return response.data.instituicoes;
+    return response.data.items;
   } catch (error) {
     console.error("Erro ao buscar instituicoes:", error);
     return null;
