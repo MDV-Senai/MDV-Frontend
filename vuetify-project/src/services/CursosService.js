446e5b1e303bb0f79ab0bd2c8b8dc0a73927043a
Matheus <matheusschmoeller@outlook.com>
2024-09-25 21:55:51 -0300

    feat: service para buscar os cursos

--- vuetify-project/src/services/CursosService.js
+++ vuetify-project/src/services/CursosService.js
@@ -2,10 +2,18 @@ import axios from "axios";
 
 export async function fetchCursos() {
   try {
+    let token = sessionStorage.getItem("authToken");
+
     const response = await axios.get(
-      "http://localhost:3000/items"
+      import.meta.env.VITE_BACKEND_URL + "/curso/instituicaoEnsino",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
     );
-    return response.data.curso;
+
+    return response.data;
   } catch (error) {
     console.error("Erro ao buscar cursos:", error);
     return null;
