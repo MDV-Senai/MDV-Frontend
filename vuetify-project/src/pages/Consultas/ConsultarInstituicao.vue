66aab033389bd39c91a2eb16cf8f298a183ec4fa
José Henrique de Oliveira do Patrocinio <henriquepatrocinio98@gmail.com>
2024-10-29 21:40:33 -0300

    feat: inst ensino modeal edicao

--- vuetify-project/src/pages/Consultas/ConsultarInstituicao.vue
+++ vuetify-project/src/pages/Consultas/ConsultarInstituicao.vue
@@ -31,7 +31,7 @@
           <tr v-for="item in instituicaoPaginada" :key="item.id">
             <td class="text-left">{{ item.nomeFantasia }}</td>
             <td class="text-left">{{ item.responsavelLegal }}</td>
-            <td class="text-left">{{ item.responsavelContato }}</td>
+            <td class="text-left">{{ item.email }}</td>
             <td class="text-center">
               <VisualizarInstituicao :instId="item.id"/>
               <EditarInstituicao :instId="item.id" />
