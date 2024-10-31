589c98057a58b4bc544e0f55bde8bfcc151c94b4
Matheus <matheusschmoeller@outlook.com>
2024-10-30 21:16:14 -0300

    feat: função criada para enviar dados para a api

--- vuetify-project/src/services/VagasService.js
+++ vuetify-project/src/services/VagasService.js
@@ -11,3 +11,22 @@ export async function fetchVagas() {
     return null;
   }
 }
+
+export async function cadastrarSolcitacaoVagas(data) {
+  try {
+    const url = import.meta.env.VITE_BACKEND_URL + "/solicitacao-vaga";
+
+    const token = sessionStorage.getItem("authToken");
+
+    const req = await axios.post(url, data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return req;
+  } catch (error) {
+    console.log('Erro ao cadastrar Solicitação de Vaga: ' + error);
+    return null;
+  }
+}
