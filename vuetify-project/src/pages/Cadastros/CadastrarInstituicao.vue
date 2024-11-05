2617302fde26e8588159edbc7604371fd41597c5
Matheus <matheusschmoeller@outlook.com>
2024-11-04 21:25:08 -0300

    feat: removendo a mascara dos campos que precisam em cadastro de inst ensino

--- vuetify-project/src/pages/Cadastros/CadastrarInstituicao.vue
+++ vuetify-project/src/pages/Cadastros/CadastrarInstituicao.vue
@@ -381,15 +381,18 @@ export default {
     },
 
     async enviarDados() {
+
+      const removeMascara = (valor) => valor ? valor.replace(/\D/g, '') : '';
+
       if (this.$refs.form.validate()) {
         const data = {
           nomeFantasia: this.instituacao,
           razaoSocial: this.razaoSocial,
-          cnpj: this.cnpj,
+          cnpj: removeMascara(this.cnpj),
           inscricaoEstadual: this.inscricaoEstadual,
-          telefone: this.telefone,
+          telefone: removeMascara(this.telefone),
           email: this.email,
-          cep: this.cep,
+          cep: removeMascara(this.cep),
           cidade: this.cidade,
           estado: this.uf,
           bairro: this.bairro,
@@ -397,24 +400,23 @@ export default {
           rua: this.logradouro,
           complemento: this.complemento,
           responsavelLegal: this.diretor,
-          responsavelLegalContato: this.contatoRespLegal,
+          responsavelLegalContato: removeMascara(this.contatoRespLegal),
         };
 
-          console.log(data);
-          const url = import.meta.env.VITE_BACKEND_URL + "/instituicao-ensino";
-          console.log(url);
+        console.log(data);
+        const url = import.meta.env.VITE_BACKEND_URL + "/instituicao-ensino";
+        console.log(url);
 
-          const token = sessionStorage.getItem('authToken');
-          console.log(token);
-          
+        const token = sessionStorage.getItem("authToken");
+        console.log(token);
 
-          const req = await axios.post(url, data, {
-            headers: {
-              'Authorization': `Bearer ${token}` 
-            }
-          });
+        const req = await axios.post(url, data, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
 
-          console.log("Resposta: ", req);
+        console.log("Resposta: ", req);
       }
     },
 
