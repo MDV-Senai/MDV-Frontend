af6ecdb1f01aeeac25d3bdee2283d630fcc051ad
José Henrique de Oliveira do Patrocinio <henriquepatrocinio98@gmail.com>
2024-11-06 21:48:10 -0300

    consultar e excluir coordenador

--- vuetify-project/src/pages/Consultas/ConsultarCoordenadorCurso.vue
+++ vuetify-project/src/pages/Consultas/ConsultarCoordenadorCurso.vue
@@ -3,7 +3,7 @@
     <Header />
     <div class="d-flex justify-center align-center">
       <v-card class="d-flex justify-center align-center" id="card_titulo">
-        <h3>Consultar Responsável Pelo Estagiário</h3>
+        <h3>Consultar Coordenador de Instituição de Ensino</h3>
       </v-card>
     </div>
     <div id="fundoCards">
@@ -22,22 +22,20 @@
         <thead>
           <tr>
             <th class="text-left">Nome</th>
-            <th class="text-left">Turma</th>
-            <th class="text-left">Curso</th>
-            <th class="text-left">Instituição</th>
+            <th class="text-left">Fone</th>
+            <th class="text-left">Email</th>
             <th class="text-center">Ações</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="item in responsavelPaginado" :key="item.id">
+          <tr v-for="item in coordenadores" :key="item.id">
             <td class="text-left">{{ item.nome }}</td>
-            <td class="text-left">{{ item.turma }}</td>
-            <td class="text-left">{{ item.curso }}</td>
-            <td class="text-left">{{ item.instituicao }}</td>
+            <td class="text-left">{{ item.fone }}</td>
+            <td class="text-left">{{ item.email }}</td>
             <td class="text-center">
               <VisualizarResponsavel />
               <EditarResponsavel />
-              <DeletarItem />
+              <DeletarItem :itemKey="'coordenador-instituicao-ensino'" :id="item.id" />
             </td>
           </tr>
         </tbody>
@@ -55,61 +53,38 @@
 </template>
 
 <script>
-import { ref, computed, onMounted, watch } from "vue";
+import { ref, onMounted, watch } from "vue";
 import { useResponsiveHeight } from "../../composables/useResponsiveHeight.js";
-import { fetchResponsaveis } from "../../services/ResponsaveisService.js";
+import { fetchCoordenadorCurso } from "../../services/CoordenadorCursoService.js";
 
 export default {
   setup() {
     const { height } = useResponsiveHeight();
-    const responsaveis = ref([]);
-    const nomeResponsavel = ref("");
+    const coordenadores = ref([]);
     const pagina = ref(1);
-    const itensPorPagina = ref(10);
-    const responsaveisFiltrados = ref([]);
+    const itensPorPagina = 10;
+    const totalPaginas = ref(1);
 
-    const loadResponsaveis = async () => {
-      const response = await fetchResponsaveis();
-      responsaveis.value = response;
-      responsaveisFiltrados.value = response;
+    const loadCoord = async () => {
+      const response = await fetchCoordenadorCurso(pagina.value, itensPorPagina);
+      coordenadores.value = response.data;
+      console.log(coordenadores.value)
+      totalPaginas.value = Math.ceil(response.total / itensPorPagina);
     };
-
-    const pesquisarResponsaveis = () => {
-      if (nomeResponsavel.value) {
-        responsaveisFiltrados.value = responsaveis.value.filter((item) =>
-          item.nome.toLowerCase().includes(nomeResponsavel.value.toLowerCase())
-        );
-      } else {
-        responsaveisFiltrados.value = responsaveis.value;
-      }
-      pagina.value = 1;
-    };
-
-    const totalPaginas = computed(() => {
-      return Math.ceil(responsaveisFiltrados.value.length / itensPorPagina.value);
-    });
-
-    const responsavelPaginado = computed(() => {
-      const start = (pagina.value - 1) * itensPorPagina.value;
-      const end = start + itensPorPagina.value;
-      return responsaveisFiltrados.value.slice(start, end);
-    });
-
-    watch(nomeResponsavel, (newValue) => {
-      pesquisarResponsaveis();
-    });
 
     onMounted(() => {
-      loadResponsaveis();
+      loadCoord();
+    });
+
+    watch(pagina, () => {
+      loadCoord();
     });
 
     return {
       height,
-      nomeResponsavel,
+      coordenadores,
       pagina,
-      itensPorPagina,
-      totalPaginas,
-      responsavelPaginado,
+      totalPaginas
     };
   },
 };
--- vuetify-project/src/services/CoordenadorCursoService.js
+++ vuetify-project/src/services/CoordenadorCursoService.js
@@ -18,3 +18,21 @@ export async function cadastrarCoordenadorCurso(data) {
       console.log('Erro ao cadastrar curso: ' + error);
     }
   }
+
+  export async function fetchCoordenadorCurso(pagina, itensPorPagina) {
+    try {
+        const response = await axios.get(
+            `${import.meta.env.VITE_BACKEND_URL}/coordenador-instituicao-ensino`,
+            {
+                params: {
+                    page: pagina,
+                    limit: itensPorPagina
+                }
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.error("Erro ao buscar coordenador:", error);
+        return null;
+    }
+}
