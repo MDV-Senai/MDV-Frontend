dcb9bd41f08f2a620e273a7a63ecd73470e1a012
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2024-11-09 11:51:58 -0300

    auth estagiario

--- vuetify-project/src/services/EstagiariosService.js
+++ vuetify-project/src/services/EstagiariosService.js
@@ -2,19 +2,23 @@ import axios from "axios";
 
 export async function fetchEstagiarios(pagina, itensPorPagina) {
   try {
-      const response = await axios.get(
-          `${import.meta.env.VITE_BACKEND_URL}/aluno`,
-          {
-              params: {
-                  page: pagina,
-                  limit: itensPorPagina
-              }
-          }
-      );
-      return response.data;
+    const token = sessionStorage.getItem('authToken');
+    const response = await axios.get(
+      `${import.meta.env.VITE_BACKEND_URL}/aluno`,
+      {
+        params: {
+          page: pagina,
+          limit: itensPorPagina
+        },
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      }
+    );
+    return response.data;
   } catch (error) {
-      console.error("Erro ao buscar alunos:", error);
-      return null;
+    console.error("Erro ao buscar alunos:", error);
+    return null;
   }
 }
 
@@ -24,10 +28,12 @@ export async function cadastrarEstagiario(data) {
 
     console.log('Dados enviados:', data.file);
     console.log('URL:', url);
+    const token = sessionStorage.getItem('authToken');
 
     const req = await axios.post(url, data, {
       headers: {
-        'Content-Type': 'multipart/form-data'
+        'Content-Type': 'multipart/form-data',
+        'Authorization': `Bearer ${token}`
       }
     });
     return req;
@@ -39,7 +45,12 @@ export async function cadastrarEstagiario(data) {
 
 export async function fetchEstagiarioPorId(estgId) {
   try {
-    const response = await axios.get(import.meta.env.VITE_BACKEND_URL + "/aluno/" + estgId);
+    const token = sessionStorage.getItem('authToken');
+    const response = await axios.get(import.meta.env.VITE_BACKEND_URL + "/aluno/" + estgId, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
 
     return response.data;
   } catch (error) {
@@ -50,8 +61,13 @@ export async function fetchEstagiarioPorId(estgId) {
 
 export async function atualizarEstagiarioPorId(estgId, data) {
   try {
+    const token = sessionStorage.getItem('authToken');
     const url = import.meta.env.VITE_BACKEND_URL + "/aluno/" + estgId;
-    const response = await axios.patch(url, data);
+    const response = await axios.patch(url, data, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
 
     return response.data;
   } catch (error) {
