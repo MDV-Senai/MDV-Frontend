6df3f969d3861b18e50f54a5052b90ada89c9310
Lucas Gabriel Tamanini Demori <lucas-demori2001@hotmail.com>
2024-11-11 19:20:59 -0300

    Feat: integração setor/estagiario

--- vuetify-project/src/services/SetoresService.js
+++ vuetify-project/src/services/SetoresService.js
@@ -23,8 +23,12 @@ export async function fetchSetoresPorId(setorId) {
 }
 
 export async function updateSetor(setorId, setor) {
+  const removeMascara = (valor) => (valor ? valor.replace(/\D/g, "") : "");
+
   try {
     const url = `${import.meta.env.VITE_BACKEND_URL}/setor/${setorId}`;
+
+    setor.telefoneCoordenador = removeMascara(setor.telefoneCoordenador);
 
     const response = await axios.patch(url, setor);
 
