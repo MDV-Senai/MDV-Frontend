ee9ac7840e6a37855bae876a3928ecd46bc69f96
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2024-11-13 01:08:48 -0300

    service solicitacao

--- vuetify-project/src/services/VagasService.js
+++ vuetify-project/src/services/VagasService.js
@@ -59,6 +59,28 @@ export async function cadastrarSolcitacaoVagas(data) {
   }
 }
 
+export async function fetchSolicitacaoVaga(pagina, itensPorPagina) {
+  try {
+    const token = sessionStorage.getItem('authToken');
+    const response = await axios.get(
+      `${import.meta.env.VITE_BACKEND_URL}/solicitacao-vaga`,
+      {
+        params: {
+          page: pagina,
+          limit: itensPorPagina
+        },
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      }
+    );
+    return response.data.data;
+  } catch (error) {
+    console.error("Erro ao buscar solicitacao:", error);
+    return null;
+  }
+}
+
 export async function fetchVagaPorId(vagaId) {
   try {
     const token = sessionStorage.getItem("authToken");
